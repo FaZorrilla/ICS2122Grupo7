27cb9bfa2aacf70c1c2e5af584e981cb05cca919
FaZorrilla <fazorrilla@uc.cl>
2020-05-06 18:33:47 -0400

    Avance minimo

--- Proyecto/simulacion.py
+++ Proyecto/simulacion.py
@@ -1,1 +1,15 @@
 #yes, esto es una simulacion
+
+class simulacion():
+    def __init__():
+        self.dia = 0
+
+    def run():
+        pass
+
+class tep(numero):
+    def __init__(numero):
+        self.numero = numero
+
+    def run():
+        pass
