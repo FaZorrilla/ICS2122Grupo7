903a3ed9d97e1a0a3dde6f3a8b5c4b9662e61cde
NatachaP <nopina@uc.cl>
2020-05-06 20:05:01 -0400

    esquema general de entidades, funciones y patio

---
+++ Elementos utiles/Lectura archivos.py
@@ -0,0 +1,18 @@
+# Eventos
+
+
+def llegada_barcos():
+    pass
+
+
+def llegada_camiones():
+    pass
+
+
+def congestion():
+    pass
+
+
+def definir_prioridad():
+    pass
+
---
+++ Elementos utiles/entidades.py
@@ -0,0 +1,26 @@
+from collections import namedtuple
+
+## BARCOS
+Barcos = namedtuple("Barcos_type", ["ide", "tamano", "lista_conteines",
+                                    "entro"])
+
+# base de datos
+lista = [("grande", 1), ("chico", 2), ("mediano", 3)]
+
+# implementación forma 1
+Lista_Barcos = map(lambda x: Barcos(*x), lista)
+
+# visualización
+print(list(Lista_Barcos))
+
+## TEPS
+TEPS = namedtuple("Teps_type", ["ide", "ocupado", "velocidad"])
+
+## CAMIONES
+Camiones = namedtuple("Camiones_type", ["ide", "capacidad", "carga"])
+
+## CONTEINERS
+
+Cointeiners = namedtuple("Conteiners_type", ["ide", "posicion", "tipo"])
+
+
