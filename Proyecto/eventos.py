d6576661697c0373a9cacfd22925efdc1fbb0baa
NatachaP <nopina@uc.cl>
2020-05-11 17:48:21 -0400

    fusion ajja

---
+++ Proyecto/eventos.py
@@ -0,0 +1,42 @@
+def congestionado(cant_vehiculos):
+    velocidad = 20 - 0.7*cant_vehiculos
+    return velocidad
+
+
+def llegada_barco(ide_barco):
+    pass
+
+
+def llamar_camion():
+    pass
+
+
+def posicionar_tep(tep):
+    pass
+
+
+def cargar(movil, tep):
+    pass
+
+
+def descargar(movil, tep):
+    pass
+
+
+def despachar_camion():
+    pass
+
+
+# ponemos reposicionar conteiner?
+
+
+def asignar_turnos():
+    pass
+
+
+def ordenar_conteines():
+    pass
+
+
+def agregar_conteiner_patio(conteiner, posicion, tep):
+    pass
